59adf3b7192e276cef58dc1ef72a7691ffc0b19c
Johcel Gene T. Bitara <jbitara@gbox.adnu.edu.ph>
2024-05-06 19:10:30 +0800

    Project refactor

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void StopGame()
+        {
+            Application.Quit();
+        }
+    }
+}
